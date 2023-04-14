f65f3199e4588c50d0abc2308d2252090e6ac253
Marco Perini <perinim.98@gmail.com>
2023-04-14 08:06:58 +0200

    import

--- import_elements.m
+++ import_elements.m
@@ -3,7 +3,7 @@ clear
 clc
 addpath('Rooms/');
 T = readtable('createGrid().csv', 'ReadVariableNames',false, 'Delimiter',',', 'HeaderLines',1, 'TreatAsEmpty',{'NA','na'});
-shape = cell(height(T),2);
+shape = cell(height(T),3);
 x_max = -1e5;
 y_max = -1e5;
 x_min = 1e5;
@@ -11,12 +11,14 @@ y_min = 1e5;
 
 %build the shape into a cell variable
 for i = 1:height(T)
-    disp(string(T{i,2}))
+    % disp(string(T{i,2}))
     x = str2num(string(T{i,2}));
     y = str2num(string(T{i,3}))*-1;
+    element_type = string(T{i,5});
     % plot([x, x(1)],[y, y(1)])
     shape{i,1} = x;
     shape{i,2} = y;
+    shape{i,3} = element_type;
 
     % takes min and max of both axes for normalization
     if x_max<max(x)
@@ -41,10 +43,9 @@ aspect_ratio = width_room/height_room;
 x_scale = [0 width_room];
 y_scale = [0 height_room];
 
-
-for j = 1:width(shape)
+for j = 1:width(shape)-1
     for i = 1:height(shape)
-        if j == width(shape)
+        if j == width(shape)-1
             % [A, B] --> [a, b] --> (val - A)*(b-a)/(B-A) + a
             shape{i,j} = (shape{i,j} - y_min)*(y_scale(2)-y_scale(1))/(y_max-y_min) + y_scale(1);
         else
@@ -59,8 +60,35 @@ hold on
 for i = 1:height(shape)
     %reconnect to initial point
     plot([shape{i,1}(1,:) shape{i,1}(1,1)], [shape{i,2}(1,:) shape{i,2}(1,1)])
-%     h=text(mean(shape{i,1}), mean(shape{i,2}), {num2str(i)});
-%     set(h,'color','r')
+end
+daspect([1 1 1])
+grid on
+grid minor
+hold off
+
+%% discretize the obstacle
+
+for i = 1:height(shape)
+    % if the element is an obstacle and the room
+    if shape{i,3} == "room" || shape{i,3} == "obstacle"
+        % discretize the obstacle
+        [x_ob, y_ob] = discretizeElement(shape{i,1}, shape{i,2}, 0.8);
+        shape{i,1} = x_ob;
+        shape{i,2} = y_ob;
+    end
+end
+
+%% plot discretized shape
+figure(2)
+hold on
+for i = 1:height(shape)
+    
+    % if the element is an obstacle and the room, plot the points
+    if shape{i,3} == "room" || shape{i,3} == "obstacle"
+        plot([shape{i,1}(1,:) shape{i,1}(1,1)], [shape{i,2}(1,:) shape{i,2}(1,1)], 'o', 'MarkerSize', 2)
+    else
+        plot([shape{i,1}(1,:) shape{i,1}(1,1)], [shape{i,2}(1,:) shape{i,2}(1,1)])
+    end
 end
 daspect([1 1 1])
 grid on
